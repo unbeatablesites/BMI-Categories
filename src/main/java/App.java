d996842c85ab582bd394fcaddfc85ae5b25a9bc4
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-17 09:28:59 -0500

    Add a questions

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,21 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        askQuestion1();
+        askQuestion2();
+
+        int heightINMeters = scan.nextInt();
 
     }
 
-    public static void askQuestion(){
-
-
+    public static void askQuestion1(){
+        System.out.println("Your height in m:");
+    }
+    public static void askQuestion2(){
+        System.out.println("Your weight in kg:");
     }
 
 }
